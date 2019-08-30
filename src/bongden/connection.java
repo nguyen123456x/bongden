936a0b73f32c348992f86ec0f823ce15f77b86a1
ntbinh <binhmobi02@gmail.com>
2019-08-30 17:17:51 +0700

    upd connection

--- src/bongden/connection.java
+++ src/bongden/connection.java
@@ -10,21 +10,23 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+
 public class connection {
+
     private Connection con;
     private Statement st;
     private ResultSet rs;
-    public Connection dbConnect(){
-     try {
+
+    public Connection dbConnect() {
+        try {
             Class.forName("com.mysql.jdbc.Driver");
-            con=DriverManager.getConnection("jdbc:mysql://localhost:3306/bongden?zeroDateTimeBehavior=convertToNull","root","");
-            System.out.println("1");
-            st=con.createStatement();
-        } catch (Exception e) {
-            System.out.println("error"+ e);
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bongden?zeroDateTimeBehavior=convertToNull", "root", "biliti02");
+            //System.out.println("1");
+            st = con.createStatement();
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("error" + e);
         }
-     return con;
-    }
-    
+        return con;
     }
 
+}
